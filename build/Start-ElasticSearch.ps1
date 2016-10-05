6f12104bbe18a8a2393d30d59ae97097c99aed4a
Blake Niemyjski <bniemyjski@gmail.com>
2016-10-05 13:31:58 -0500

    Fixed elasticsearch script

--- build/Start-ElasticSearch.ps1
+++ build/Start-ElasticSearch.ps1
@@ -10,15 +10,15 @@ If ($env:JAVA_HOME -eq $null -or !(Test-Path -Path $env:JAVA_HOME)) {
 
 Push-Location $PSScriptRoot
 
-If (!(Test-Path -Path "elasticsearch-$es_version") -And !(Test-Path -Path "elasticsearch.zip")) {
+If (!(Test-Path -Path "elasticsearch-$es_version") -And !(Test-Path -Path "elasticsearch-$es_version.zip")) {
     Invoke-WebRequest "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$es_version.zip" -OutFile "elasticsearch-$es_version.zip"
 }
 
-If ((Test-Path -Path "elasticsearch.zip") -And !(Test-Path -Path "elasticsearch-$es_version")) {
+If ((Test-Path -Path "elasticsearch-$es_version.zip") -And !(Test-Path -Path "elasticsearch-$es_version")) {
     Add-Type -assembly "system.io.compression.filesystem"
     [io.compression.zipfile]::ExtractToDirectory("$PSScriptRoot\elasticsearch-$es_version.zip", $PSScriptRoot)
     cp .\elasticsearch.yml .\elasticsearch-$es_version\config -Force
-    rm elasticsearch.zip
+    rm elasticsearch-$es_version.zip
 }
 
 Start-Process -NoNewWindow "$(Get-Location)\elasticsearch-$es_version\bin\elasticsearch.bat"
