638aeff5e2271d7028fed83543b2a80c2fd2a0af
Eric J. Smith <eric@codesmithtools.com>
2016-11-29 17:45:57 -0600

    Fix crappy async issue in the visitors.

--- tests/Foundatio.Parsers.LuceneQueries.Tests/IncludeQueryVisitorTests.cs
+++ tests/Foundatio.Parsers.LuceneQueries.Tests/IncludeQueryVisitorTests.cs
@@ -9,6 +9,7 @@ using Nest;
 using Xunit;
 using Xunit.Abstractions;
 using Foundatio.Parsers.ElasticQueries.Extensions;
+using System.Threading.Tasks;
 
 namespace Foundatio.Parsers.Tests {
     public class IncludeQueryVisitorTests : TestWithLoggingBase {
@@ -55,22 +56,35 @@ namespace Foundatio.Parsers.Tests {
 
         [Fact]
         public void CanExpandElasticIncludes() {
-            var client = new ElasticClient();
-            var includes = new Dictionary<string, string> { { "other", "field:value" } };
-            var aliases = new AliasMap { { "field", "aliased" } };
+            var client = new ElasticClient(new ConnectionSettings().DisableDirectStreaming().PrettyJson());
+            var aliases = new AliasMap { { "field", "aliased" }, { "included", "aliasedincluded" } };
 
-            var processor = new ElasticQueryParser(c => c.UseIncludes(includes).UseAliases(aliases));
+            var processor = new ElasticQueryParser(c => c.UseIncludes(i => GetInclude(i)).UseAliases(aliases));
             var result = processor.BuildQueryAsync("@include:other").Result;
             var actualResponse = client.Search<MyType>(d => d.Index("stuff").Query(q => result));
             string actualRequest = actualResponse.GetRequest();
             _logger.Info($"Actual: {actualRequest}");
 
-            var expectedResponse = client.Search<MyType>(d => d.Index("stuff").Query(f => f.Term("aliased", "value")));
+            var expectedResponse = client.Search<MyType>(d => d.Index("stuff").Query(f => f.Bool(b => b.Filter(f1 => f1.Term("aliasedincluded", "value")))));
             string expectedRequest = expectedResponse.GetRequest();
             _logger.Info($"Expected: {expectedRequest}");
 
             Assert.Equal(expectedRequest, actualRequest);
             Assert.Equal(expectedResponse.Total, actualResponse.Total);
+
+            result = processor.BuildQueryAsync("@include:other").Result;
+            actualResponse = client.Search<MyType>(d => d.Index("stuff").Query(q => result));
+            actualRequest = actualResponse.GetRequest();
+            _logger.Info($"Actual: {actualRequest}");
+            _logger.Info($"Expected: {expectedRequest}");
+
+            Assert.Equal(expectedRequest, actualRequest);
+            Assert.Equal(expectedResponse.Total, actualResponse.Total);
+        }
+
+        private async Task<string> GetInclude(string name) {
+            await Task.Delay(150);
+            return "included:value";
         }
     }
 }
