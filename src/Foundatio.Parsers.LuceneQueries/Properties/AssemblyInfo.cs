9b3e7a13573bd8187e60fa0797dfc324445f2941
Eric J. Smith <eric@codesmithtools.com>
2017-03-07 08:45:04 -0800

    Convert to new csproj

--- src/Foundatio.Parsers.LuceneQueries/Properties/AssemblyInfo.cs
+++ src/Foundatio.Parsers.LuceneQueries/Properties/AssemblyInfo.cs
@@ -1,19 +1,3 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Foundatio.Parsers.LuceneQueries")]
-[assembly: AssemblyDescription("A lucene style query parser that is extensible and allows additional syntax features.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Exceptionless")]
-[assembly: AssemblyProduct("Exceptionless")]
-[assembly: AssemblyCopyright("Copyright (c) 2016 Exceptionless.  All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-
-[assembly: InternalsVisibleTo("Foundatio.Parsers.Tests")]
-
+[assembly: InternalsVisibleTo("Foundatio.Parsers.LuceneQueries.Tests")]
