cbf01bb97e3fc6e54dde8b0c80c90f2966fb65ba
Blake Niemyjski <bniemyjski@gmail.com>
2019-12-18 08:58:02 -0600

    Going to move the checks up to repos.

--- src/Foundatio.Parsers.ElasticQueries/Extensions/DefaultAggregationNodeExtensions.cs
+++ src/Foundatio.Parsers.ElasticQueries/Extensions/DefaultAggregationNodeExtensions.cs
@@ -165,8 +165,7 @@ namespace Foundatio.Parsers.ElasticQueries.Extensions {
             var end = GetDate(context, "EndDate");
             
             bool hasValidStartBoundDate = start.HasValue && start.Value > DateTime.MinValue && start.Value < DateTime.MaxValue;
-            bool hasValidEndBoundDate = end.HasValue && end.Value < DateTime.MaxValue;
-            var bounds = hasValidStartBoundDate && hasValidEndBoundDate ? new ExtendedBounds<DateMath> { Minimum = start.Value, Maximum = end.Value } : null;
+            var bounds = hasValidStartBoundDate && end.HasValue ? new ExtendedBounds<DateMath> { Minimum = start.Value, Maximum = end.Value } : null;
 
             var interval = GetInterval(proximity, start, end);
             var agg = new DateHistogramAggregation(originalField) {
