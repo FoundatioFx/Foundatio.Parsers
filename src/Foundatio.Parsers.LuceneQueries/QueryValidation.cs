e25ee605df6f4d314098e58bd0724b79629f3311
Eric J. Smith <eric@codesmithtools.com>
2022-03-07 17:22:37 -0600

    Update deps

--- src/Foundatio.Parsers.LuceneQueries/QueryValidation.cs
+++ src/Foundatio.Parsers.LuceneQueries/QueryValidation.cs
@@ -17,7 +17,7 @@ public class QueryValidationOptions {
 
 [DebuggerDisplay("IsValid: {IsValid} Message: {Message} Type: {QueryType}")]
 public class QueryValidationResult {
-    private ConcurrentDictionary<string, ICollection<string>> _operations = new ConcurrentDictionary<string, ICollection<string>>(StringComparer.OrdinalIgnoreCase);
+    private ConcurrentDictionary<string, ICollection<string>> _operations = new(StringComparer.OrdinalIgnoreCase);
 
     public string QueryType { get; set; }
     public bool IsValid => ValidationErrors.Count == 0;
